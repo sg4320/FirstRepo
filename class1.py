2da4fbeb052c7d0376da0b8a2337209258fc0936
sg4320 <165339987+sg4320@users.noreply.github.com>
2024-04-04 19:51:07 -0400

    1

---
+++ class1.py
@@ -0,0 +1,23 @@
+myfruit=['apple','orange','grape','banana']
+for x in myfruit:
+    print(x)
+
+for i in range(4):
+    print(myfruit[i])
+
+mynumberlist=[1,2,3,4,5,6]
+for x in mynumberlist:
+    print(x)
+
+for y in range(2,11):
+    print(y)
+
+def mysum(a,b):
+    return(a+b)
+
+first = 1
+second = 2
+print(mysum(first,second))
+
+color=['red','orange','purple','yellow']
+
--- newfile.py
+++ newfile.py
@@ -4,3 +4,8 @@ print("new try")
 for i in range(5):
     print(i)
     i+1
+
+i = 1
+while i <= 10: 
+    print(i)
+    i+=1
